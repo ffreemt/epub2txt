a91f42e898b930844fa80f4cff0e9caffcb7b15b
ffreemt <yucongo+fmt@gmail.com>
2023-06-05 17:10:42 +0800

    Bump version from 0.1.2-alpha.0 to 0.1.2, from collections.abc import Iterable

--- epub2txt/__init__.py
+++ epub2txt/__init__.py
@@ -1,5 +1,5 @@
 """Init."""
 from .epub2txt import epub2txt
 
-__version__ = "0.1.2a0"
+__version__ = "0.1.2"
 __all__ = ("epub2txt",)
--- epub2txt/epub2txt.py
+++ epub2txt/epub2txt.py
@@ -7,10 +7,10 @@ from pathlib import Path
 
 # for with_func_attrs
 # from typing import Iterable
-try:
-    from collections import Iterable  # < py38
-except ImportError:
-    from collections.abc import Iterable
+# try:
+# from collections import Iterable  # < py38
+# except ImportError:
+from collections.abc import Iterable
 
 # the rest
 from itertools import zip_longest
