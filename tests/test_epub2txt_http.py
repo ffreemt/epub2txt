76a22d700b42a2c02a8172bc6be7395850083071
ffreemt <yucongo+fmt@gmail.com>
2023-06-11 16:41:32 +0800

    Update epub2txt.content_titles, intended for metadata when needed

--- tests/test_epub2txt_http.py
+++ tests/test_epub2txt_http.py
@@ -5,7 +5,8 @@ from epub2txt import epub2txt
 
 def test_epub2txt_http():
     """Test epub2txt_http."""
-    url = "https://github.com/ffreemt/tmx2epub/raw/master/tests/1.tmx.epub"
+    # url = "https://github.com/ffreemt/tmx2epub/raw/master/tests/1.tmx.epub"
+    url = "https://github.com/ffreemt/epub2txt/raw/master/tests/1.tmx.epub"
 
     res = epub2txt(url)
     assert len(res) > 220000
